69aabe5b840178ff0afc4211bde78fc76db42183
Denis Meshalkin <egoden@tut.by>
2019-08-27 11:26:09 +0300

    add: finish dropLodash method

--- homework-custom-lodash/drop.js
+++ homework-custom-lodash/drop.js
@@ -1,14 +1,32 @@
 const shiftMethod = function(array) {
-  if (array.length > 0) {
+  const arr = [...array];
+  let resultArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    resultArr[i] = arr[i + 1];
   }
+  resultArr.length--;
+  return resultArr;
 };
 
 const drop = function dropMethod(array, number = 1) {
-  console.log(number);
-  console.log(array);
+  let result = [];
   if (array.length <= number) {
     return [];
   }
+  if (number === 0) {
+    return array;
+  }
+  if (array.length > number) {
+    for (let j = 0; j < number; j++) {
+      if (result.length > 0) {
+        result = shiftMethod(result);
+      }
+      if (result.length === 0) {
+        result = shiftMethod(array);
+      }
+    }
+  }
+  return result;
 };
 
-console.log(drop([1, 2, 3, 4, 5], 3));
+console.log(drop([1, 2, 3, 4, 5], 6));
