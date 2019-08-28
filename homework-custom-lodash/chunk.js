88bb14b55ea34f8ab42daf2089db43a3d427a16a
Denis Meshalkin <egoden@tut.by>
2019-08-28 15:52:23 +0300

    add: slice method

--- homework-custom-lodash/chunk.js
+++ homework-custom-lodash/chunk.js
@@ -3,11 +3,29 @@ const pushMethod = function(array, value) {
   array[nextElement] = value;
 };
 
-const spliceMethod = function(array, size) {
-  const arr = [...array];
-  arr.length = size;
-  console.log(arr);
-  return arr;
+const spliceMethod = function(array, start, end) {
+  let length = array == null ? 0 : array.length;
+  if (!length) {
+    return [];
+  }
+  start = start == null ? 0 : start;
+  end = end === undefined ? length : end;
+  if (start < 0) {
+    start = -start > length ? 0 : length + start;
+  }
+  end = end > length ? length : end;
+  if (end < 0) {
+    end += length;
+  }
+  length = start > end ? 0 : (end - start) >>> 0;
+  start >>>= 0;
+
+  let index = -1;
+  const result = new Array(length);
+  while (++index < length) {
+    result[index] = array[index + start];
+  }
+  return result;
 };
 
 const shiftMethod = function(array) {
