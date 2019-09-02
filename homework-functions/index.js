96bb97cba578862501539f83ff5e817ba4b305a2
Denjcproduct <42620183+Denjcproduct@users.noreply.github.com>
2019-09-02 17:15:46 +0300

    add: autoComplete functions with trie structure (#5)
    * add: autoComplete functions with trie structure
    
    * add: case insensetive in autoComplete Function
    
    * refactor: use classes instead of functions
    
    * fix: change in case insensetive logics
    
    * add: count in trie and repeat word in array
    
    * add: custom-chunk-functions
    
    * add: compact method function
    
    * add: start a create dropLodash method
    
    * add: finish dropLodash method
    
    * add: takeLodash function
    
    * add: splice method
    
    * add: slice method
    
    * fix: chunkLodash function
    
    * fix: rewrite take function with slice
    
    * refactor:
    recorded array methods in a separate file
    
    * add: jest and some tests
    
    * add: dropWhile test and method
    
    * add: pick method and start pickBy method
    
    * add:filter,includes,map,omit,zip
    
    * delete: pickBy methods
    
    * refactor: some fix in autocomplete
    
    * delete: lodash functions  from functions branche
    
    * refactor: change findAllWords function

---
+++ homework-functions/index.js
@@ -0,0 +1,100 @@
+class TrieNode {
+  constructor(key) {
+    this.key = key;
+    this.parent = null;
+    this.children = {};
+    this.end = false;
+    this.count = null;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode(null);
+  }
+
+  insert(word, count) {
+    let node = this.root;
+    for (let i = 0; i < word.length; i++) {
+      if (!node.children[word[i]]) {
+        node.children[word[i]] = new TrieNode(word[i]);
+        node.children[word[i]].parent = node;
+      }
+      node = node.children[word[i]];
+      if (i == word.length - 1) {
+        node.end = true;
+        node.count = count;
+      }
+    }
+  }
+
+  find(prefix) {
+    let node = this.root;
+    let output = [];
+    for (let i = 0; i < prefix.length; i++) {
+      if (node.children[prefix[i]]) {
+        node = node.children[prefix[i]];
+      } else {
+        return output;
+      }
+    }
+    return findAllWords(node);
+  }
+}
+
+function getWord(trieNode) {
+  let output = [];
+  let node = trieNode;
+  while (node !== null) {
+    output.push(node.key);
+    node = node.parent;
+  }
+  output.reverse();
+  return output.join("");
+}
+
+function findAllWords(node, arr = []) {
+  let resultArr = arr;
+  if (node.end) {
+    for (let k = 0; k < node.count; k++) {
+      resultArr.push(getWord(node));
+    }
+  }
+  for (let child in node.children) {
+    findAllWords(node.children[child], resultArr);
+  }
+  return resultArr;
+}
+
+function capitalizeFirstLetter(string, toCapital = true) {
+  return toCapital
+    ? string.charAt(0).toUpperCase() + string.slice(1)
+    : string.charAt(0).toLowerCase() + string.slice(1);
+}
+
+function createAutoComplete(array) {
+  const trie = new Trie();
+  const counts = array.reduce(function(obj, cur) {
+    if (!obj[cur]) {
+      obj[cur] = 0;
+    }
+    obj[cur]++;
+    return obj;
+  }, {});
+  const nonRepeatWords = [...new Set(array)];
+  nonRepeatWords.forEach(word => {
+    let wordCount = counts[word];
+    trie.insert(word, wordCount);
+  });
+  return function(prefix) {
+    if (!prefix || prefix === "") {
+      return [];
+    }
+    const prefixInLowerCase = capitalizeFirstLetter(prefix, false);
+    const capitalizePrefix = capitalizeFirstLetter(prefix);
+    const lowerResult = trie.find(prefixInLowerCase);
+    const upperResult = trie.find(capitalizePrefix);
+    const result = lowerResult.concat(upperResult);
+    return result;
+  };
+}
