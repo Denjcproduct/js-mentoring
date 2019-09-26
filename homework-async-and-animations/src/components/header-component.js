c90c52f679834cb054bd358d11808f81fee0aa45
Denis Meshalkin <egoden@tut.by>
2019-09-26 23:38:12 +0300

    style: fix in chat styles

--- homework-async-and-animations/src/components/header-component.js
+++ homework-async-and-animations/src/components/header-component.js
@@ -2,16 +2,18 @@ import createElement from '../helpers/createElement';
 
 const createHeader = function header() {
   const chatHeader = createElement('div', 'chat__header');
-  const chatLogo = createElement('div', 'chat__logo');
+  const logoContainer = createElement('div', 'chat__logoContainer');
+  const logo = createElement('img', 'chat__logo');
   const chatName = createElement('div', 'chat__name');
   const chat = document.querySelector('#chat');
-  const logoText = document.createTextNode('CHAT');
   const userName = localStorage.getItem('chatUserName');
   const nameText = userName
     ? document.createTextNode(userName)
     : document.createTextNode('DefaultName');
-  chatLogo.appendChild(logoText);
-  chatHeader.appendChild(chatLogo);
+  logo.setAttribute('alt', 'Logo');
+  logo.setAttribute('src', './assets/icons/logo.svg');
+  logoContainer.appendChild(logo);
+  chatHeader.appendChild(logoContainer);
   chatHeader.appendChild(chatName);
   chatName.appendChild(nameText);
   chat.appendChild(chatHeader);
