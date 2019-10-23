f291ce709c089848d1c8ec4831dab5b68dd68ef6
Denis Meshalkin <egoden@tut.by>
2019-10-23 16:35:17 +0300

    add: chunks in request

--- homework-node/helpers/deleteFile.js
+++ homework-node/helpers/deleteFile.js
@@ -3,8 +3,12 @@ const { promisify } = require('util');
 
 const deleteFileAsync = promisify(fs.unlink);
 
-const deleteFile = (pathToFile) => {
-  deleteFileAsync(pathToFile);
+const deleteFile = async (pathToFile) => {
+  try {
+    await deleteFileAsync(pathToFile);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = deleteFile;
--- homework-node/index.js
+++ homework-node/index.js
@@ -1,13 +1,17 @@
 const getUrls = require('./helpers/readFile');
 const deleteFile = require('./helpers/deleteFile');
 const getDataAndWriteFile = require('./helpers/getPageSpeedAndWriteResults');
+const chunk = require('../homework-custom-lodash/chunk');
 
 async function app(fileWithUrl, fileWithResult) {
   try {
     await Promise.all([deleteFile(fileWithResult), deleteFile('./error.txt')]);
     const urls = await getUrls(fileWithUrl);
-    for (const url of urls) {
-      await getDataAndWriteFile(url, fileWithResult, './error.txt');
+    const urlChunks = chunk(urls, 5);
+    for (const urlChunk of urlChunks) {
+      await Promise.all(
+        urlChunk.map((url) => getDataAndWriteFile(url, fileWithResult, './error.txt'),),
+      );
     }
   } catch (error) {
     console.log(error);
