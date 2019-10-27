892fa4257a73b53dec57b3560558a05fa6182be0
Denis Meshalkin <egoden@tut.by>
2019-10-27 23:12:18 +0300

    start: create a site

---
+++ homework-cssGrid/webpack.config.js
@@ -0,0 +1,29 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: ['./app.js', './scss/main.scss'],
+  output: {
+    filename: 'dist/bundle.js'
+  },
+  module: {
+    rules: [
+      /*
+      your other rules for JavaScript transpiling go in here
+      */
+      {
+        // css / sass / scss loader for webpack
+        test: /\.(css|sass|scss)$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      // define where to save the file
+      filename: 'dist/css/[name].bundle.css',
+      allChunks: true
+    })
+  ]
+};
